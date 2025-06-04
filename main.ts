0f0544b2f8a9e8d68103e0be71067e63190780d6
iopo-code <2432088193@qq.com>
2025-06-04 21:16:12 +0800

    V0.5

--- main.ts
+++ main.ts
@@ -2,13 +2,3 @@ import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
 // 启动静态服务器
 Deno.serve({ port: 8000 }, (req) => serveDir(req, { fsRoot: "." }));
-
-// 仅在本地环境自动打开浏览器
-if ("Command" in Deno) {
-  const command = new Deno.Command("cmd", {
-    args: ["/c", "start", "http://localhost:8000/"],
-    stdout: "null",
-    stderr: "null"
-  });
-  command.spawn();
-}
